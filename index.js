c3448f8a1e36e2d285c2fee82060578d95e2affa
Annette  Drapalski <annette.drapalski@gmail.com>
2016-10-24 04:06:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+    const rankUp = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+    for (let i = 0, l = rankUp.length; i < l; i++) {
+      rankUp[i].innerHTML = parseInt(rankUp[i].innerHTML) + n;
+    }
+}
+
+function deepestChild() {
+  const deepC = document.getElementById('grand-node').querySelectorAll('div');
+  var hold = [];
+  var result;
+  for (let i = 0, l = deepC.length; i < (l-1); i++) {
+    hold.push(deepC[i]);
+  }
+  return hold.pop();
+}
